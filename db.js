2538e8fbd3b87513669236f07d62177478320a5e
Creigh Battle <creighbattle@gmail.com>
2021-07-12 16:36:32 -0400

    connection-string

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
-require("dotenv").config();
-
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  database: process.env.DATABASE,
-  idleTimeoutMillis: 0,
-  connectionTimeoutMillisL: 0,
-  // connectionString: process.env.DATABASE_URL,
+  // username: process.env.USERNAME,
+  // password: process.env.PASSWORD,
+  // host: process.env.HOST,
+  // port: process.env.PORT,
+  // database: process.env.DATABASE,
+  // idleTimeoutMillis: 0,
+  // connectionTimeoutMillisL: 0,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
