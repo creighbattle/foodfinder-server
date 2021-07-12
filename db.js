ca5e14173323245d15e322c48da56498842f7545
Creigh Battle <creighbattle@gmail.com>
2021-07-12 01:32:06 -0400

    server commit 5

--- db.js
+++ db.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  database: process.env.DATABASE,
+  // username: process.env.USERNAME,
+  // password: process.env.PASSWORD,
+  // host: process.env.HOST,
+  // port: process.env.PORT,
+  // database: process.env.DATABASE,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
