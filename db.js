b26cb5b5b3461f8deadb4a0772827fc6300bb6f9
Creigh Battle <creighbattle@gmail.com>
2021-07-12 21:09:47 -0400

    updatingdb

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  // user: "doadmin",
-  // password: "ka65somv9jb3sz2i",
-  // host: "db-postgresql-nyc3-40928-do-user-9293543-0.b.db.ondigitalocean.com",
-  // port: 25060,
-  // database: "defaultdb",
-  connectionString: process.env.DATABASE_URL,
+  user: "hnwsvblncjnqcf",
+  password: "1c05f0a1f7c9f259027979240068b9af5d8d2923a00c7701e5588add28dba1c3",
+  host: "ec2-3-224-7-166.compute-1.amazonaws.com",
+  database: "d9ng0oj5rinbb8",
+ 
 });
+
+
 
 module.exports = pool;
