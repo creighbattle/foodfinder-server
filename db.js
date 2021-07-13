248fd67cf6da3bb49e7d745936ed0efda6e6e378
Creigh Battle <creighbattle@gmail.com>
2021-07-12 21:27:55 -0400

    updatingdb2

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ const pool = new Pool({
   password: "1c05f0a1f7c9f259027979240068b9af5d8d2923a00c7701e5588add28dba1c3",
   host: "ec2-3-224-7-166.compute-1.amazonaws.com",
   database: "d9ng0oj5rinbb8",
- 
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-
 
 module.exports = pool;
