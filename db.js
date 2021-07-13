403379ae9180491149eeb599c4eb1f4c001c8553
Creigh Battle <creighbattle@gmail.com>
2021-07-12 20:41:27 -0400

    db3

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: 'db',
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: 25060,
-  database: 'db',
-  ssl: 'disabled'
-  //connectionString: process.env.DATABASE_URL
+  // user: "doadmin",
+  // password: "ka65somv9jb3sz2i",
+  // host: "db-postgresql-nyc3-40928-do-user-9293543-0.b.db.ondigitalocean.com",
+  // port: 25060,
+  // database: "defaultdb",
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
